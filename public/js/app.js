cf3d32b928a85d4e3613068258f1f9375dc08f59
Oussama Jebrane <oussamajebrane98@gmail.com>
2023-06-08 12:54:57 +0100

    added list elements

--- public/js/app.js
+++ public/js/app.js
@@ -2,33 +2,46 @@ let createInput = document.querySelector("#create-list");
 let btnList = document.querySelector("#add-list");
 let allListDiv = document.querySelector(".all-lists")
 
-function addItem() {
+const addList = () => {
     // check if the input value is different than 0
     if (createInput.value != "") {
         //create tags for the column
-        let div = document.createElement("div");
+        let divParent = document.createElement("div");
+        let divChild = document.createElement("div");
         let button = document.createElement("button");
+        let input = document.createElement("input");
         let h1 = document.createElement("h1");
         //add class to tags
-        div.setAttribute(
+        divParent.setAttribute(
             "class",
-            "bg-danger h-100 w-25  rounded-3 d-flex flex-column align-items-center ms-4 me-4 p-4 box-container"
-        );
-        button.setAttribute("class", "btn btn-oussama mb-4 add-task");
-        h1.setAttribute("class", "text-white mb-4");
+            "zone list-item bg-danger rounded-3 align-items-center ms-4 me-4 p-4"
+        )
+
+        h1.setAttribute("class", "text-white mb-4")
+        button.setAttribute("class", "add-task btn btn-oussama mb-4 w-100");
+        input.setAttribute("class", "taskInput");
+        divChild.setAttribute("class", "d-flex flex-column gap-2");
 
         //add text to the button and title
         h1.innerHTML = createInput.value;
         button.innerHTML = "Add a task";
-        //appendChild to the column
-        allListDiv.append(div);
-        div.append(h1, button);
+
+        //append to the column
+        allListDiv.append(divParent);
+        divChild.append(input, button)
+        divParent.append(h1, divChild);
         //reset the input value
         createInput.value = "";
-        //call the add task event listener function
     }
 }
 
-btnList.addEventListener("click", addItem);
+btnList.addEventListener("click", addList);
+
+
+const addTask = () => {
+    
+}
+
+
 
 
