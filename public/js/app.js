edb64da566cc7fe6f04e5d66376a0e074575eb6c
Oussama Jebrane <oussamajebrane98@gmail.com>
2023-06-09 08:18:49 +0100

    yeaaah got the drag and drop to work

--- public/js/app.js
+++ public/js/app.js
@@ -15,22 +15,22 @@ const addList = () => {
         let btnAdd = document.createElement("button");
         // div to fix the UpDown problem: tasks went waay up
         let divTasks = document.createElement("div")
-        
+
         // button for delete the list
         let btnDelete = document.createElement("button");
         let h2 = document.createElement("h2");
-        
+
         // add classes to my tags
         divParent.setAttribute(
             "class",
             "zone list-item bg-primary rounded-3 align-items-center ms-4 me-4 p-4"
-            )
-            
-            h2.setAttribute("class", "text-white mb-4")
-            btnAdd.setAttribute("class", "add-task btn btn-oussama mb-4 w-100");
-            btnDelete.setAttribute("class", "deleteList")
-            divChild.setAttribute("class", "d-flex flex-column gap-2");
-            divTasks.setAttribute("class", "taskHolder");
+        )
+
+        h2.setAttribute("class", "text-white mb-4")
+        btnAdd.setAttribute("class", "add-task btn btn-oussama mb-4 w-100");
+        btnDelete.setAttribute("class", "deleteList")
+        divChild.setAttribute("class", "d-flex flex-column gap-2");
+        divTasks.setAttribute("class", "taskHolder");
 
         //add text to the buttons and title
         h2.innerHTML = createInput.value;
@@ -67,23 +67,21 @@ const addTask = (btn) => {
     // ask for task name and verify it's not empty
     let taskInput = prompt("Add a task: ").trim();
 
-    while(taskInput == ""){
+    while (taskInput == "") {
         taskInput = prompt("Add a task: ").trim();
     }
 
 
     // create my div for the task
     let newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "draggable bg-light");
     // give the possibility of dragging
-    newDiv.setAttribute("draggable", true);
 
+    newDiv.setAttribute("class", "draggable");
+    newDiv.setAttribute("draggable", true);
     // task items : 
     // paragraph with textContent as our inputted task name
     // buttons that contain icons that will allow us to modify our tasks
     newDiv.innerHTML += `
-                        <div class="d-flex justify-content-between p-2">
-
                         <p class="m-0">${taskInput}</p>
 
                         <div>
@@ -97,8 +95,6 @@ const addTask = (btn) => {
 
                         <button class="icon"><i class="delete fa-solid fa-trash" style="color: #ff0000;"></i></button>
                         </div>
-
-                    </div>
                     `
     // add our entire new div to the task list in the divTasks div
     btn.nextElementSibling.insertAdjacentElement("afterbegin", newDiv);
@@ -123,9 +119,9 @@ const modify = (btn) => {
 
 const down = (btn) => {
     // Select the div that contains the task and everything
-    let task = btn.parentElement.parentElement.parentElement.parentElement;
+    let task = btn.parentElement.parentElement.parentElement;
     // check if there's an element after it or not
-    if (task.nextElementSibling != null){
+    if (task.nextElementSibling != null) {
         // put the div after the next element
         task.nextElementSibling.insertAdjacentElement("afterend", task);
     }
@@ -133,7 +129,7 @@ const down = (btn) => {
 
 const up = (btn) => {
     // select the div that contains the task and everything
-    let task = btn.parentElement.parentElement.parentElement.parentElement;
+    let task = btn.parentElement.parentElement.parentElement;
     // check if there's an element before it or not
     if (task.previousElementSibling != null) {
         // put the div before the previous element
@@ -141,11 +137,40 @@ const up = (btn) => {
     }
 }
 
+const dragDrop = (item) => {
+    // when dragging starts
+    let zones = document.querySelectorAll(".zone");
+    let draggables = document.querySelectorAll(".draggable");
+    item.addEventListener("dragstart", () => {
+        // make our item opaque
+        item.classList.add("dragging");
+    })
+
+    // when done dragging an item
+    item.addEventListener("dragend", () => {
+        // return the opacity of our item
+        item.classList.remove("dragging");
+    })
+
+    zones.forEach(thing => {
+        // event listneder when our dragged item is over our zone
+        thing.addEventListener("dragover", eve => {
+            // prevent our mouse icon from looking like a stop sign
+            eve.preventDefault();
+            // only one item can have dragging class 
+            let item = document.querySelector(".dragging");
+            // we add that child to the div that we are currently "draging over"
+            // it automatically goes to the last position
+            thing.append(item)
+        })
+    })
+}
 
 // click events
 document.onclick = function (event) {
     // select what i'm clicking on
     let target = event.target;
+    console.log(target.classList[0]);
     // check the first class of our targeted element
     // if it has a certain class then call for the appropriate function
     switch (target.classList[0]) {
@@ -169,6 +194,9 @@ document.onclick = function (event) {
             break;
         case "deleteList":
             deleteList(target);
+        case "draggable":
+            dragDrop(target);
+            break;
     }
 }
 
